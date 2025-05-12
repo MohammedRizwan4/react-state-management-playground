a9e78a854503fae40712aebe1afbae0aa0ec7522
Mohammed Rizwan Bhavnagri <92419569+MohammedRizwan4@users.noreply.github.com>
2025-05-12 19:04:47 +0530

    feat: using react-redux

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Page as ReactDefault } from "./react/Page";
 import { Page as ReactLessRender } from "./react-less-render/Page";
 import { Page as ReactContextExample } from "./react-context/Page";
+import { Page as ReactReduxExample } from "./redux/Page";
 
 function App() {
   return (
@@ -11,11 +12,13 @@ function App() {
         <Link to="/react">React</Link>
         <Link to="/react-less-render">React less render</Link>
         <Link to="/react-context">React Context</Link>
+        <Link to="/react-redux">React Redux</Link>
       </nav>
       <Routes>
         <Route path="react" element={<ReactDefault />} />
         <Route path="/react-less-render" element={<ReactLessRender />} />
         <Route path="/react-context" element={<ReactContextExample />} />
+        <Route path="/react-redux" element={<ReactReduxExample />} />
       </Routes>
     </BrowserRouter>
   );
